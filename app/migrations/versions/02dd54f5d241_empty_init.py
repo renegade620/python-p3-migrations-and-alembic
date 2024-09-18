55e53108afb9189b786425d8ac49c0fcdc000253
Frankline Were <franklinegift@gmail.com>
2024-09-18 12:00:25 +0300

    master alembic

---
+++ app/migrations/versions/02dd54f5d241_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 02dd54f5d241
+Revises: 
+Create Date: 2024-09-18 11:41:18.256875
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '02dd54f5d241'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
